e6e6914b38bdb53b8245381562cbfb1ec91f3a0c
jamesh <jamesh39@outlook.com>
2020-12-06 18:38:10 -0800

    updated with a java class

--- js/script.js
+++ js/script.js
@@ -15,161 +15,275 @@ For assistance:
 const maxDisplayItems = 9;
 const maxPage = (data.length / maxDisplayItems) + 1;
 
-//keep track of the last clicked button
-var lastClickedButton = null;
+//A class to control the page, starts off initializing the page and displaying it
+//has helper functions to manupulate the page
+class PageController {
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-function showPage(list, page) {
+   //private properties
 
-   //lets make sure we get valid data before processing
-   if(list != null && page > 0 && page <= maxPage) {
-      // create two variables which will represent the index for the first and last student on the page
-      let startIndex = ((page * maxDisplayItems) - maxDisplayItems);
-      let endIndex = (page * maxDisplayItems);
+   //the student list html builder
+   #studentListHTML = null;
+   //empty student array
+   #studentList = [];
 
-      // select the element with a class of `student-list` and assign it to a variable
-      const studentList = document.querySelector('.student-list');
-      //lets make sure we grabbed the correct element
-      if(studentList != null) {
-         // set the innerHTML property of the variable you just created to an empty string
-         studentList.innerHTML = '';
+   //keep track of the last clicked button
+   #lastClickedButton = null; 
 
-         // loop over the length of the `list` parameter
-         for(var index = 0; index < list.length; index++) {
-            // inside the loop create a conditional to display the proper students
-            if(index >= startIndex && index < endIndex) {
-            // inside the conditional:
-               // create the elements needed to display the student information
+   //keep track of pagination initialization, we dont want to redo it if its already happened
+   #paginationOnce = false;
 
-               //the outer list element item
-               let studentItem = document.createElement("LI");
-               studentItem.className = "student-item cf";
+   //constructor, initializes the page HTML
+   constructor() {
+      this.#studentListHTML = document.querySelector('.student-list');
+      //locally store the list in data.js
+      this.#studentList = data;
 
-               //the div element
-               let divElement = document.createElement("DIV");
-               divElement.className = "student-details";
+   }
 
-               //the avatar image element
-               let imgElement = document.createElement("IMG");
-               imgElement.className = "avatar";
+   isLastClickedButtonValid() {
+      return (this.#lastClickedButton != null);
+   }
+   setLastClickedButtonClassName(className) {
+      this.#lastClickedButton.className = className;
+   }
+   setLastClickedButtonObject(buttonObject) {
+      this.#lastClickedButton = buttonObject;
+   }
+   getLastClickedButtonIndex() {
+      return parseInt(this.#lastClickedButton.textContent);
+   }
 
-               //the image itself and set attribute
-               const image = list[index].picture.large;
-               imgElement.setAttribute("src", `${image}`);
+   //private function to build HTML list items
+   #buildListItems(list, start, end) {
+       //lets make sure we grabbed the correct element in the constructor
+       if(this.#studentListHTML != null && list != null) {
+          //make sure we have valid start and end
+          if(start >= 0 && end > start) {
+            // set the innerHTML property of the variable you just created to an empty string
+            this.#studentListHTML.innerHTML = '';
+            // loop over the length of the `list` parameter
+            for(var index = 0; index < list.length; index++) {
+               // inside the loop create a conditional to display the proper students
+               if(index >= start && index < end) {
+                  // inside the conditional:
+                  // create the elements needed to display the student information
 
-               //the H3 element and context set
-               let h3Element = document.createElement("H3");
-               h3Element.textContent = `${list[index].name.first}  ${list[index].name.last}`;
+                  //the outer list element item
+                  let studentItem = document.createElement("LI");
+                  studentItem.className = "student-item cf";
 
-               //the span element and context set
-               let spanElement = document.createElement("SPAN");
-               spanElement.className = "email";
-               spanElement.textContent = `${list[index].email}`;
+                  //the div element
+                  let divElement = document.createElement("DIV");
+                  divElement.className = "student-details";
 
-               let divJoinElement = document.createElement("DIV");
-               divJoinElement.className = "joined-details";
+                  //the avatar image element
+                  let imgElement = document.createElement("IMG");
+                  imgElement.className = "avatar";
 
-               let spanDateElement = document.createElement("SPAN");
-               spanDateElement.className = "date";
-               spanDateElement.textContent = `Joined: ${list[index].registered.date}`;
+                  //the image itself and set attribute
+                  const image = list[index].picture.large;
+                  imgElement.setAttribute("src", `${image}`);
 
-               // insert the above elements
-               divElement.appendChild(imgElement);
-               divElement.appendChild(h3Element);
-               divElement.appendChild(spanElement);
-               studentItem.appendChild(divElement);
-               
-               divJoinElement.appendChild(spanDateElement);
-               studentItem.appendChild(divJoinElement);
+                  //the H3 element and context set
+                  let h3Element = document.createElement("H3");
+                  h3Element.textContent = `${list[index].name.first}  ${list[index].name.last}`;
 
-               //studentList.insertAdjacentHTML('beforeend', studentItem);
-               studentList.appendChild(studentItem);
+                  //the span element and context set
+                  let spanElement = document.createElement("SPAN");
+                  spanElement.className = "email";
+                  spanElement.textContent = `${list[index].email}`;
+
+                  let divJoinElement = document.createElement("DIV");
+                  divJoinElement.className = "joined-details";
+
+                  let spanDateElement = document.createElement("SPAN");
+                  spanDateElement.className = "date";
+                  spanDateElement.textContent = `Joined: ${list[index].registered.date}`;
+
+                  // insert the above elements
+                  divElement.appendChild(imgElement);
+                  divElement.appendChild(h3Element);
+                  divElement.appendChild(spanElement);
+                  studentItem.appendChild(divElement);
+                  
+                  divJoinElement.appendChild(spanDateElement);
+                  studentItem.appendChild(divJoinElement);
+
+                  this.#studentListHTML.appendChild(studentItem);
+               }
             }
+
+            return true;
+         }
+      }
+      return false;
+   }
+
+   /*
+   Create the `showPage` function
+   This function will create and insert/append the elements needed to display a "page" of nine students
+   */
+   showPage(list, page) {
+
+      //lets make sure we get valid data before processing
+      if(page > 0 && page <= maxPage) {
+         // create two variables which will represent the index for the first and last student on the page
+         let startIndex = ((page * maxDisplayItems) - maxDisplayItems);
+         let endIndex = (page * maxDisplayItems);
+
+         let newList = list;
+
+         if(newList === null) {
+            newList = this.#studentList;
+         }
+         //call the build items function
+         let result = this.#buildListItems(newList, startIndex, endIndex);
+
+         if(result) {
+            //make sure we only call the addPagination once
+            if(this.#paginationOnce == false) {
+               this.#addPagination(this.#studentList);
+            }
+         } else {
+            alert("Failed to create list items.");
+         }
+      }
+   }
+
+   #LabelElementEventHandler(eventObject, self) {
+      if(eventObject.target.nodeName === "BUTTON") {
+         const textField = document.getElementById("search");
+         
+         if(textField != null) {
+            const userText = textField.value.toLowerCase();
+            
+            //lets make sure we have a valid string
+            if(userText.length > 0) {
+               //declare new array
+               var newList= [];
+               //track how many were found
+               let foundCount = 0;
+
+               for(var index = 0; index < self.#studentList.length; index++) {
+                  //get the last name string from the index in list, force the name to lowercase for easy searching
+                  let name = self.#studentList[index].name.last.toLowerCase();
+                  
+                  //check if the name includes the user text
+                  if(name.includes(userText)) {
+                     newList[foundCount] = self.#studentList[index];
+                     foundCount++;
+                  }
+               }
+               //finally, show the new list
+               if(foundCount > 0) {
+                  self.showPage(newList, 1);
+               }
+
+            }
+            else{
+               alert("No Names found!");
+            }
+         }
+         else {
+            alert("invalid text field!")
+         }
+      }
+   }
+
+   #ListItemEventHandler(eventObject, self) {
+      // if the click target is a button:
+      if(eventObject.target.nodeName === "BUTTON") {
+         // remove the "active" class from the previous button
+         if(self.isLastClickedButtonValid()) {
+            self.setLastClickedButtonClassName('');
+         }
+
+         // add the active class to the clicked button
+         self.setLastClickedButtonObject(eventObject.target);
+         self.setLastClickedButtonClassName("active");
+
+         // call the showPage function passing the `list` parameter and page to display as arguments
+         const page = self.getLastClickedButtonIndex();
+         self.showPage(page);
+      }
+   }
+   /*
+   Create the `addPagination` function
+   This function will create and insert/append the elements needed for the pagination buttons
+   */
+   #addPagination(list) {
+
+      //lets make sure the list is valid before processing it
+      if(list != null) {
+
+         //GOING FOR EXCEEDS
+         const labelElement = document.createElement("LABEL");
+         labelElement.setAttribute("for", "search");
+         labelElement.className = "student-search";
+
+         const inputElement = document.createElement("INPUT");
+         inputElement.setAttribute("id", "search");
+         inputElement.setAttribute("placeholder", "Search by name...");
+
+         const searchButton = document.createElement("BUTTON");
+         const searchBtnImg = document.createElement("IMG");
+         searchBtnImg.setAttribute("src", "img/icn-search.svg");
+         searchBtnImg.setAttribute("alt", "Search icon");
+         searchButton.appendChild(searchBtnImg);
+
+         labelElement.onclick = (eventObject) => {
+            this.#LabelElementEventHandler(eventObject, this);
+         }
+
+         labelElement.appendChild(inputElement);
+         labelElement.appendChild(searchButton);
+
+         document.querySelector("h2").appendChild(labelElement);
+         //END EXCEEDS
+
+
+         // create a variable to calculate the number of pages needed
+         const numOfPages = Math.ceil(list.length / maxDisplayItems);
+
+         // select the element with a class of `link-list` and assign it to a variable
+         let linkList = document.querySelector(".link-list");
+
+         // set the innerHTML property of the variable you just created to an empty string
+         linkList.innerHTML = '';
+
+         // loop over the number of pages needed
+         for(var index = 0; index < numOfPages; index++) {
+
+            // create the elements needed to display the pagination button
+            let listItem = document.createElement("LI");
+            
+            //create the button element
+            let button = document.createElement("BUTTON");
+            button.textContent = `${index + 1}`;
+
+            // give the first pagination button a class of "active"
+            if(index == 0) {
+               button.className = "active";
+               this.#lastClickedButton = button;
+               this.#paginationOnce = true;
+            }
+
+            // insert the above elements
+            listItem.appendChild(button);
+            linkList.appendChild(listItem);
+         }
+
+         // create an event listener on the `link-list` element
+         linkList.onclick = (eventObject) => {
+              this.#ListItemEventHandler(eventObject, this);
          }
 
       } else {
-         alert("Failed to get student list element!");
+         alert("Pagination failed: the list parameter is null!");
       }
-   } else if(list === null) {
-      alert("invalid list item passed to first parameter!");
    }
-   else if(page < 1) {
-      alert("please enter a page number that is at least 1 or greater!");
-   }
-   else if(page > maxPage) {
-      alert("Page number is greater than the number of pages that can be displayed!");
-   }
- }
+}
  
-
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-function addPagination(list) {
-
-   //lets make sure the list is valid before processing it
-   if(list != null) {
-      // create a variable to calculate the number of pages needed
-      const numOfPages = Math.ceil(list.length / maxDisplayItems);
-
-      // select the element with a class of `link-list` and assign it to a variable
-      let linkList = document.querySelector(".link-list");
-
-      // set the innerHTML property of the variable you just created to an empty string
-      linkList.innerHTML = '';
-
-      // loop over the number of pages needed
-      for(var index = 0; index < numOfPages; index++) {
-
-         // create the elements needed to display the pagination button
-         let listItem = document.createElement("LI");
-         
-         //create the button element
-         let button = document.createElement("BUTTON");
-         button.textContent = `${index + 1}`;
-
-         // give the first pagination button a class of "active"
-         if(index == 0) {
-            button.className = "active";
-            lastClickedButton = button;
-         }
-
-         // insert the above elements
-         listItem.appendChild(button);
-         linkList.appendChild(listItem);
-      }
-
-      // create an event listener on the `link-list` element
-      linkList.addEventListener('click', (eventObject) => {
-         // if the click target is a button:
-         if(eventObject.target.nodeName === "BUTTON") {
-            // remove the "active" class from the previous button
-            if(lastClickedButton != null) {
-               lastClickedButton.className = '';
-            }
-
-            // add the active class to the clicked button
-            lastClickedButton = eventObject.target;
-            lastClickedButton.className = "active";
-
-            // call the showPage function passing the `list` parameter and page to display as arguments
-            const page = parseInt(lastClickedButton.textContent);
-            showPage(list, page);
-         }
-      });
-
-   } else {
-      alert("Pagination failed: the list parameter is null!");
-   }
- }
- 
-
-
 // Call functions
-addPagination(data);
-showPage(data, 1);
+var page = new PageController();
+page.showPage(null, 1);
